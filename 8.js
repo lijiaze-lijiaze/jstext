79bf360af6fc2c729a327c085da08edf080c1c3c
lijiaze <lijiaze1010@outlook.com>
2020-07-26 20:04:48 +0800

    js数据类型map的使用-基于两数只和算法

--- 8.js
+++ 8.js
@@ -1,1 +1,24 @@
-console.log(~-1)
+function twosum(arr,target){
+    var aaa = new Map()
+    aaa.set('a',10)
+    
+     arr.forEach((x,index) => {
+         console.log(aaa.has(x))
+         if(aaa.has(x)){
+             console.log(index,aaa.get(x))
+             return
+         }else{
+             aaa.set(x,index)
+         }
+     });
+    // for(let i = 0;i<arr.length;i++){
+    //     console.log(aaa.has(arr[i]))
+    //     if(aaa.has(arr[i])){
+    //         console.log(i,aaa.get(arr[i]))
+    //         return
+    //     }else{
+    //         aaa.set(arr[i],i)
+    //     }
+    // }
+}
+twosum([1,2,5,3,5],3)
