8a2dbae23d97c6b574ae93047fc718ea624b3c34
lijiaze <lijiaze1010@outlook.com>
2020-07-26 21:13:32 +0800

    算法之无重复子字符串

---
+++ 9.js
@@ -0,0 +1,21 @@
+function aaa(str){
+    const setarr = new Set()
+    let i = 0,j = 0,max = 0
+    for(i;i<str.length;i++){
+        if(setarr.has(str[i])){
+            while(setarr.has(str[i])){
+               setarr.delete(str[j])
+                j++
+                
+            }
+            setarr.add(str[i])
+            
+        }else{
+            setarr.add(str[i])
+            max = Math.max(max,setarr.size)
+        }
+    }
+    console.log(max)
+    return max
+}
+aaa('abcdacba')
