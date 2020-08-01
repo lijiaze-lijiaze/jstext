3523a5c494abb1681e35a60bf68d2d3e0b35760b
lijiaze <lijiaze1010@outlook.com>
2020-08-01 21:34:27 +0800

    js高阶函数

---
+++ js高阶函数.js
@@ -0,0 +1,16 @@
+//实现一个repeat方法
+function repeat(func,times,wait){
+    if(typeof times !=='number')return
+    if(typeof wait !=='number')return
+    return function(str){
+        for(let i = 0;i<times;i++){
+            setTimeout(()=>{
+                function(str)
+            },i*wait)
+        }
+    }
+
+}
+//使下面的代码能够正常工作
+const repeatFunc = repeat(console.log,4,3000)
+repeatFunc('hello')//会输出四次hello，每次间隔三秒
---
+++ requestldleCallback和requestAnimatonFrame有什么区别.js
@@ -0,0 +1,3 @@
+//requestldleCallback和requestAnimatonFrame有什么区别
+//requestldleCallback属于低优先级任务
+//requestAnimatonFrame属于高优先级任务，会在每一帧确定执行
