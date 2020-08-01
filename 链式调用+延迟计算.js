8a676faa3d9dc97d851b13ed6977a4866834e03c
lijiaze <lijiaze1010@outlook.com>
2020-08-01 22:21:26 +0800

    链式调用+延迟计算

---
+++ 链式调用+延迟计算.js
@@ -0,0 +1,15 @@
+//写一个加法函数sum,支持sum(1)(2)(3,4)输出10
+function sum(...args){
+    function next(...innerArgs){
+        args.push(...innerArgs);
+        return next
+    }
+    next.valueOf = ()=>{
+        return args.reduce((r,c) => r+c,0)
+    }
+    next.toString = ()=>{
+        return args.reduce((r,c) => r+c,0)
+    }
+    return next
+}
+sum(1)(2)
