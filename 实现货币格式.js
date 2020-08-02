a4452433f4f3656d72be748178b82c728e5644a7
lijiaze <lijiaze1010@outlook.com>
2020-08-02 16:07:12 +0800

    货币格式

---
+++ 实现货币格式.js
@@ -0,0 +1,3 @@
+//612334353转化为612,334,353
+(612334353).toLocaleString(en-US)
+(612334353).toString().split('').reverse().join('').replace(/\d{3}/g,function($1){return $1+','}).split(''),reverse().join()
