59fe92cf9cf5e51b196b6e97d539ebd1b3d2723b
lijiaze <lijiaze1010@outlook.com>
2020-08-02 20:58:52 +0800

    dom的一些操作

---
+++ 修改dom的方法.js
@@ -0,0 +1,8 @@
+createDocumentFregment()//创建一个DOM片段
+creatElement()//创建一个具体元素
+createTextNode()//创建一个文本节点
+cloneNode()//克隆节点
+appendChild()//添加节点
+insertBefore()
+removeChild()//移除节点
+replaceChild()//查找节点
