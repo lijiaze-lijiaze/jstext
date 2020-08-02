faadccd6322b409e5af7a118ef5d9f9a9fc83c58
lijiaze <lijiaze1010@outlook.com>
2020-08-02 10:10:39 +0800

    js循环机制

--- js循环机制.js
+++ js循环机制.js
@@ -8,3 +8,30 @@
 //宏任务微任务
 //宏任务：整体代码，settimeout,setinterval,setimmediatle,I/O,UIrender,
 //微任务：promise
+async function async1(){
+    console.log('async1 start')
+    await async2()
+    console.log('async1 end')
+}
+async function async2(){
+    console.log('async2 start')
+    return new Promise((resolve,reject)=>{
+        resolve();
+        console.log('async2 promise')
+    })
+
+}
+console.log('illegalscript start')
+setTimeout(function(){
+    console.log('setTimeout')
+},0)
+async1()
+new Promise(function(resolve){
+    console.log('promise1')
+    resolve()
+}).then(function(){
+    console.log('promise2')
+}).then(function(){
+    console.log('promise3')
+})
+console.log('illegalscript end')
