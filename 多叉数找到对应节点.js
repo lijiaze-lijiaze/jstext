ab4e31d4958972643877345c9f71f6f9c71206e6
lijiaze <lijiaze1010@outlook.com>
2020-08-02 14:38:35 +0800

    多叉数遍历

---
+++ 多叉数找到对应节点.js
@@ -0,0 +1,56 @@
+var cityData = [
+    {
+        id: 1,
+        name: 广东,
+        children: [
+            {
+                id: 2,
+                name: 北京,
+                children: [
+                    {
+                        id: 3,
+                        name:海淀,
+                    }
+                ]
+            }
+        ]
+    }
+]
+//根据ID找到name
+let result = ''
+const recursion = (cityData,id) => {
+    if(!cityData||!cityData.length) return;
+    for(let i = 0 ,len = cityData.length;i<len;i++){
+        const childs = cityData[i].children
+        if(cityData[i].id == id){
+            result = cityData[i].name
+        }
+        if(child && childs.length>0){
+            recursion(childs,id)
+        }
+    }
+    return result
+}
+
+//广度优先遍历
+let result1 = ''
+
+const range = (cityData,id) => {
+    if(!cityData || !cityData.length) return;
+    let stack = [];
+    let item = null
+    for(let i = 0 ,len = cityData.length;i<len;i++){
+        stack.push(cityData[i]);
+    }
+    while(stack.length){
+        item = stack.shift();
+        if(item.id === id){
+            result = item.name
+        }
+        if(item.children && item.children.length){
+            stack = stack.concat(item.children)
+            //stack = item.children.concat(stack)如此写即为深度优先
+        }
+    }
+    return result
+}
