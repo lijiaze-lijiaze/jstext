f3236f59d6742ae01a80fde264e80ad567d2c3e2
lijiaze <lijiaze1010@outlook.com>
2020-08-02 18:06:58 +0800

    实现一个jsonp

--- 实现一个jsonp.js
+++ 实现一个jsonp.js
@@ -1,12 +1,40 @@
-const kcallBackMap = {}
-function uuid () {
-    return 
-}
-function jsonp(url,callback){
-    const callbackId = uuid()
-    url += 'callback=' + callbackId;
-    window[callbackId] = callback
-    const script = document.createElement('script');
-    script.src = url;
-    document.head.appendChild(script)
+var jsonp = function({url,params,cb,timeout}){
+    return new Promise((resolve,reject)=>{
+        const script = document.createElement('script')
+        const sdName = 'jsonp_'+Date.now()
+        const urlObj = new URL(url)
+        let timeOutCallback = false
+        urlObj.searchParams.append('callback',cbName)
+        params && Object.keys(params).forEach(paramsKey=>{
+            urlObj.searchParams.append(paramsKey,params[paramsKey])
+        })
+        script.src = urlObj.toString()
+        const commonCb = ()=>{
+            delete window[cbName]
+            document.body.removeChild(script)
+        }
+        script.onerror = function(event){
+            if(!timeOutCallback){
+                cb && cb(event)
+                reject(event)
+            }
+            commonCb()
+        }
+        window[cbName] = function(data){
+            if(!timeOutCallback){
+                cb && cb(event)
+                reject(event)
+            }
+            commonCb()
+        }
+        if(timeout !==undefined){
+            setTimeout(()=>{
+                timeOutCallback = true
+                const resp = {msg:'timeout'}
+                cb && cb(resp)
+                reject(resp)
+            },timeout)
+        }
+        document.body.appendChild(script)
+    })
 }
