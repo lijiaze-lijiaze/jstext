d1dd58d456ec163808c68709ae3440e1f9c51c1e
lijiaze <lijiaze1010@outlook.com>
2020-08-03 19:15:13 +0800

    css3的一些动画

---
+++ css3新属性.js
@@ -0,0 +1,19 @@
+//新增选择器（：not(.input))
+//圆角
+//多列布局
+//阴影和反射
+//文字特效
+//文字渲染
+//线性渐变
+//旋转
+
+//transform:
+//translate(x,y) 2d转化
+//translate3d(x,y,z) 3d转化
+//translateX(x) x轴上移动
+//tanslateY(y)  y轴上移动
+//translateZ(z) 
+//scale(x[,y]?) 缩放
+//scale3d(x,y,z) 3d缩放
+//rotate(angle) 旋转
+//rotateX(angle) 沿着x轴3d旋转
