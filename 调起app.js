8ffcf5ffaffc1e37f58f5417888989b075a37b64
lijiaze <lijiaze1010@outlook.com>
2020-08-03 13:37:32 +0800

    调起app

---
+++ 调起app.js
@@ -0,0 +1,8 @@
+//web调起app
+//ios9以上使用universal link
+//ios9一下使用iframe+scheme形式，如何判断iframe是否成功打开，通过timer以及pageshow和pagehide
+//安卓一般通过iframe+scheme
+
+//小程序调起App，可以通过open-type的值设置为launchApp的button的点击来触发
+//小程序调起小程序，只有统一公众号关联的小程序才能跳转
+Wx.navigatetominigrograme
