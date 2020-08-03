6ceaafdd0962d615f8ed53b3838c30824aaab62e
lijiaze <lijiaze1010@outlook.com>
2020-08-03 14:09:48 +0800

    foreach执行异步操作

---
+++ async&foreach.js
@@ -0,0 +1,24 @@
+const list = [1,2,3]
+const square = num => {
+    return new Promise((resolve,reject)=>{
+        setTimeout(() => {
+            resolve(num*num)
+        }, 1000);
+    })
+}
+function test(){
+    list.forEach(async x => {
+        const res = await square(x)
+        console.log(res)
+    })
+}
+//一秒后输出1，4，9
+async function test(){
+    for(let x of list){
+        const res = await square(x)
+        console.log(res)
+    }
+}
+
+//for循环中包含异步调用时会等异步调用完成后再执行后面的语句（先输出1、2、3最后输出completed）
+//forEach中包含异步调用时会直接执行后面的语句（先输出completed，然后输出1、2、3）
