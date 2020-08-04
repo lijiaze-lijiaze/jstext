1f04929a684a3710a23a2f751aa7cfec06c81dcd
lijiaze <lijiaze1010@outlook.com>
2020-08-04 16:39:46 +0800

    判断质数

---
+++ 判断一个数是否是质数.js
@@ -0,0 +1,18 @@
+function isPrime(number){
+    if(typeof number !=='number' || !Number.isInteger(number)){return false}
+    if(number<2){
+        return false
+    }
+    if(number == 2){
+        return true
+    }else if(number%2 ==0){
+        return false
+    }
+    var squareRoot = Math.sqrt(number)
+    for(var i = 3; i<= squareRoot;i+=2){
+        if(number%i == 0){
+            return false
+        }
+    }
+    return true
+}
