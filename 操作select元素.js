c2c5cd9ac29ec8add9b3fffe2b56b659ba3c3dd7
lijiaze <lijiaze1010@outlook.com>
2020-08-05 13:46:33 +0800

    操作select

--- 操作select元素.js
+++ 操作select元素.js
@@ -9,8 +9,21 @@ function addOption(){
      objSelect.options.add(new Option('文本2','值2'))
 }
 addOption()
-//删除
+//删除所有
 function remove(){
     var objSelect = document.querySelector('#mySelect');
     objSelect.options.length = 0
+}
+//删除当前
+function removenow(){
+    var objSelect = document.querySelector('#mySelect')
+    var index = objSelect.selectedIndex
+    objSelect.options.remove(index)
+}
+//获取当前option的内容
+function getNow(){
+    var objSelect = document.querySelector('#mySelect')
+    var index = objSelect.selectedIndex
+    var nowValue = objSelect.options[index].value
+    console.log(nowValue)
 }
