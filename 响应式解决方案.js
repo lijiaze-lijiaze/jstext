5dd98bb66b29197eb275ab249e673e5cb229b81a
lijiaze <lijiaze1010@outlook.com>
2020-08-05 13:55:37 +0800

    响应式

---
+++ 响应式解决方案.js
@@ -0,0 +1,23 @@
+//media query
+//@media screen and (max-width: 960px){
+//     body{
+//         background-color:#FF6699
+//       }
+//   }
+
+
+//百分比
+// 1）子元素height和width的百分比
+// 子元素的height或width中使用百分比，是相对于子元素的直接父元素
+// 2）padding,margin
+// 子元素的padding如果设置百分比，不论是垂直方向或者是水平方向，都相对于直接父亲元素的width，而与父元素的height无关。
+
+//flex
+
+//rem
+// 在响应式布局中，必须通过js来动态控制根元素font-size的大小
+//postcss-loader
+
+//js动态计算
+//css像素	为web开发者提供，在css中使用的一个抽象单位
+//物理像素	只与设备的硬件密度有关，任何设备的物理像素都是固定的
