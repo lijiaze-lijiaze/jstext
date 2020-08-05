28868a8afaae25bd895767b5effee8517901b556
lijiaze <lijiaze1010@outlook.com>
2020-08-05 15:34:56 +0800

    继承相关

--- js实现多继承.js
+++ js实现多继承.js
@@ -17,4 +17,46 @@ Object.assign(S.prototype,M2.prototype)
 S.prototype.constructor = S;
  var s = new S()
  console.log(s) 
- 
+ //----------------------------------------------
+ //js的几种继承方式，也写在这里
+ //构造函数
+ function Parent(age){
+    this.name = 'mike'
+    this.age = age
+ }
+ function Child(age){
+    Parent.call(this,age)
+ }
+ var test = new Child(21)
+ //原型链继承
+ function Parent(){
+     this.name = 'mike'
+ }
+ function Child(){
+     this.age = 12
+ }
+ Child.prototype = new Parent()
+ //组合继承
+ function Parent(age){
+     this.name = 'mike'
+     this.age = age
+ }
+ Parent.prototype.run = function(){
+     return this.name + '666' +this.age
+ }
+ function Child(age){
+     Parent.call(this,age)
+ }
+ Child.prototype = new Parent()
+ var test = new Child(21)
+ //原型式继承
+ function obj(o){
+     function F(){}
+     F.prototype = o
+     return new F()
+ }
+ var box = {
+     name:'box1',
+     arr:[1,2]
+ }
+ var b1 = obj(box)
