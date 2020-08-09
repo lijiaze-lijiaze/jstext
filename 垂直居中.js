de8acaed9419f9092fed0c553faa7f4b1505d000
lijiaze <lijiaze1010@outlook.com>
2020-08-09 13:32:29 +0800

    垂直居中

---
+++ 垂直居中.js
@@ -0,0 +1,28 @@
+// table布局
+// <div id="wrapper">
+// 	<div id="cell">
+// 		<div class="content">Content goes here</div>
+// 	</div>
+// </div>
+// #wrapper {
+// 	display: table;
+// }
+
+// #cell {
+// 	display: table-cell;
+// 	vertical-align: middle;
+// }
+// 绝对定位
+// <div class="content"> Content goes here</div>
+// #content {
+// 	position: absolute;
+// 	top: 50%;
+// 	height: 240px;
+// 	margin-top: -120px; /* negative half of the height */
+// }
+// flex
+// parentElement{
+//     display:flex;/*Flex布局*/
+//     display: -webkit-flex; /* Safari */
+//     align-items:center;/*指定垂直居中*/
+// }
