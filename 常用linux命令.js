2d0694eb8a5eab3d15d129e264e37bfc7618ef59
lijiaze <lijiaze1010@outlook.com>
2020-08-17 22:39:58 +0800

    iostat用于输出CPU和磁盘I/O相关的统计信息

---
+++ 常用linux命令.js
@@ -0,0 +1,12 @@
+
+//sort, Linux sort命令用于将文本文件内容加以排序
+//uniq Linux uniq 命令用于检查及删除文本文件中重复出现的行列
+
+//find,Linux find 命令用来在指定目录下查找文件
+//grep,Linux grep 命令用于查找文件里符合条件的字符串。
+//awk,AWK 是一种处理文本文件的语言，是一个强大的文本分析工具
+//sed Linux sed 命令是利用脚本来处理文本文件。
+
+//top, Linux top命令用于实时显示 process 的动态。
+//netstat, Linux netstat 命令用于显示网络状态。
+//iostat iostat用于输出CPU和磁盘I/O相关的统计信息
