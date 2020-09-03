087693fd54a51ac1cde55971dc49acbcdf226fe0
lijiaze <lijiaze1010@outlook.com>
2020-09-03 22:14:29 +0800

    z字形拼接字符串

--- z字形拼接字符串.js
+++ z字形拼接字符串.js
@@ -1,30 +1,38 @@
 let s = "LEETCODEISHIRING", numRows = 3
 function change(str,num){
-let arr = new Array().fill()
-for(let j = 0;j<3;j++){
-    arr.push([])
-}
-let count = 0
-let rise = true
-for(item in str){
-    console.log(count)
-    arr[count].push(str[item])
-    if(rise){
-        count++
-       
-        if(count == numRows-1){
-            rise=false 
-        }
-    }else{
-        count--
-        if(count == 0){
-            rise=true 
+
+    let arr = new Array().fill()
+    for(let j = 0;j<numRows;j++){
+        arr.push([])
+    }
+    console.log(arr)
+    let count = 0
+    let rise = true
+    if(numRows==1){
+    let result = str
+    return result
+    }
+    for(item in str){
+        console.log(count)
+        
+           arr[count].push(str[item])
+        
+        if(rise){
+            count++
+           
+            if(count == numRows-1){
+                rise=false 
+            }
+        }else{
+            count--
+            if(count == 0){
+                rise=true 
+            }
         }
     }
-}
-
-let result = arr.reduce((abc,xyz)=>[...abc,...xyz])
-
-console.log('result',result)
+    
+    let result = arr.reduce((abc,xyz)=>[...abc,...xyz])
+    return result.join('')
+    //console.log('result',result)
 }
 change(s,numRows)
