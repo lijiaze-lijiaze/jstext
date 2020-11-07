049b87c7c2bf03ba5c2e219254cc60ff9f6035fc
lijiaze <lijiaze1010@outlook.com>
2020-11-07 14:53:32 +0800

    链式调用终极版

--- 链式调用.js
+++ 链式调用.js
@@ -11,4 +11,43 @@ return function(){
     }
 }
 }
-sum(1)(2,3)(4,5,6)
+//sum(1)(2,3)(4,5,6)
+
+
+function sum2(){
+
+    let arr = Array.prototype.slice.call(arguments)
+    console.log(arr)
+    return function(){
+        let arr2 = Array.prototype.slice.call(arguments)
+        return sum2.apply(null,arr.concat(arr2))
+    }
+    }
+//sum2(1)(2,3)(4,5,6)
+
+function sum3(){
+
+    let arr = Array.prototype.slice.call(arguments)
+    console.log(arr)
+    let fn = function(){
+        let arr2 = Array.prototype.slice.call(arguments)
+        return sum3.apply(null,arr.concat(arr2))
+    }
+    return fn
+    }
+//sum3(1)(2,3)(4,5,6)
+
+function sum4(){
+
+    let arr = Array.prototype.slice.call(arguments)
+    console.log(arr)
+    let fn = function(){
+        let arr2 = Array.prototype.slice.call(arguments)
+        return sum4.apply(null,arr.concat(arr2))
+    }
+    fn.toString=function(){
+        return arr.reduce((acc,cur)=>acc+cur,0)
+    }
+    return fn
+    }
+sum4(1)(2,3)(4,5,6)
