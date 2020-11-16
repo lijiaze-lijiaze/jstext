cc545dc9243660e8a6271624676312e09dabe89c
lijiaze <lijiaze1010@outlook.com>
2020-11-16 09:24:40 +0800

    深拷贝

--- 实现深拷贝.js
+++ 实现深拷贝.js
@@ -20,3 +20,51 @@ function deepClone(obj){
     }
     return result
 }
+
+function deepCopy3(obj) {
+    // hash表，记录所有的对象的引用关系
+    let map = new WeakMap();
+    function dp(obj) {
+      let result = null;
+      let keys = Object.keys(obj);
+      let key = null,
+        temp = null,
+        existobj = null;
+   
+      existobj = map.get(obj);
+      //如果这个对象已经被记录则直接返回
+      if(existobj) {
+        return existobj;
+      }
+   
+      result = {}
+      map.set(obj, result);
+   
+      for(let i =0,len=keys.length;i<len;i++) {
+        key = keys[i];
+        temp = obj[key];
+        if(temp && typeof temp === 'object') {
+          result[key] = dp(temp);
+        }else {
+          result[key] = temp;
+        }
+      }
+      return result;
+    }
+    return dp(obj);
+  }
+   
+  const obj= {
+    a: {
+      name: 'a'
+    },
+    b: {
+      name: 'b'
+    },
+    c: {
+   
+    }
+  };
+  c.d.e = obj.a;
+   
+  const copy = deepCopy3(obj);
